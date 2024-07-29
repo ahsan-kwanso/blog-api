e3eb9270a72906b056ec8700bb8368d9aba51dc1
ahsan-kwanso <m.ahsan@kwanso.com>
2024-07-29 11:38:51 +0500

    status codes added for auth controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,20 @@
 import { signUpUser, signInUser } from "../services/auth.service.js";
+import { statusCodes } from "../utils/statusCodes.js";
 
 const signUp = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const result = await signUpUser(name, email, password);
     if (!result.success) {
-      return res.status(400).json({ message: result.message });
+      return res
+        .status(statusCodes.BAD_REQUEST)
+        .json({ message: result.message });
     }
-    return res.status(201).json({ token: result.token });
+    return res.status(statusCodes.CREATED).json({ token: result.token });
   } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
+    return res
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
   }
 };
 
@@ -20,12 +23,14 @@ const signIn = async (req, res) => {
   try {
     const result = await signInUser(email, password);
     if (!result.success)
-      return res.status(400).json({ message: result.message });
-    return res.status(201).json({ token: result.token });
+      return res
+        .status(statusCodes.UNAUTHORIZED)
+        .json({ message: result.message });
+    return res.status(statusCodes.OK).json({ token: result.token });
   } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
+    return res
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
   }
 };
 

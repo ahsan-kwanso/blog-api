07824c12341cef8b99f89874464b59ea878e334f
ahsan-kwanso <m.ahsan@kwanso.com>
2024-07-30 15:01:53 +0500

    dbsync not recommended

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import { sequelize } from "./sequelize/config/sequelize.js";
 import router from "./routes/index.js";
 
 dotenv.config();
@@ -14,16 +13,3 @@ app.get("/", (req, res) => {
 });
 
 app.use(router);
-
-const dbSync = async () => {
-  try {
-    await sequelize.sync();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-
-dbSync();
